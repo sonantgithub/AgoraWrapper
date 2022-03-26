9942cd35253bfb3c0a8eb3c55838e1fa89c8480d
sonantgithub <sonantgithub>
2022-03-26 14:59:47 +0530

    2nd commit 26 march

--- standAloneAgora/src/main/java/com/example/standaloneagora/Toasty.java
+++ standAloneAgora/src/main/java/com/example/standaloneagora/Toasty.java
@@ -4,7 +4,8 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class Toasty {
-    private void simpleMsg (Context c ,String s){
+    private static void simpleMsg (Context c ,String s)
+    {
         Toast.makeText( c,s, Toast.LENGTH_SHORT).show();
     }
 }
