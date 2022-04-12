fe145d81bb50c538653d9aaaded148b781bd0c7b
sonantgithub <sonantgithub>
2022-04-12 14:31:02 +0530

    9 commit 12 april

--- standAloneAgora/src/main/java/com/example/standaloneagora/Toasty.java
+++ standAloneAgora/src/main/java/com/example/standaloneagora/Toasty.java
@@ -8,12 +8,14 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 
 public class Toasty {
     Context contextlib;
+    DatabaseReference reference = FirebaseDatabase.getInstance("https://signeystreamingdb.firebaseio.com/").getReference();
 
     public void simpleMsg(Context context, String text) {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
@@ -27,7 +29,7 @@ public class Toasty {
         hashMap.put("test", "test");
 
 
-        FirebaseDatabase.getInstance("https://signeystreamingdb.firebaseio.com/").getReference().child("testing").updateChildren(hashMap)
+        reference.child("testing").updateChildren(hashMap)
                 .addOnCompleteListener(new OnCompleteListener() {
                     @Override
                     public void onComplete(@NonNull Task task) {
