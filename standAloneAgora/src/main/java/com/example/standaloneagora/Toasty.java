4dadd148225a6872ca292f77d1589535566f3326
sonantgithub <sonantgithub>
2022-04-12 10:21:00 +0530

    1 commit 12 april

--- standAloneAgora/src/main/java/com/example/standaloneagora/Toasty.java
+++ standAloneAgora/src/main/java/com/example/standaloneagora/Toasty.java
@@ -3,11 +3,32 @@ package com.example.standaloneagora;
 import android.content.Context;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 public class Toasty {
 
     public void simpleMsg(Context context, String text) {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
     }
+
+    public void testingofFirebase() {
+        HashMap hashMap = new HashMap();
+        hashMap.put("test", "test");
+
+
+        FirebaseDatabase.getInstance("https://signeystreamingdb.firebaseio.com/").getReference().child("testing").updateChildren(hashMap)
+                .addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+
+                    }
+                });
+
+    }
 }
-
-
